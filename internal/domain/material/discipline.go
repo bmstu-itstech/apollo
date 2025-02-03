7a60c4360e1d300b5c5d7ba4eb1af4fa55d73fbb
Andrey Nikitin <andrew@a71.su>
2025-02-03 22:57:03 +0300

    Improve type constructors

--- internal/domain/material/discipline.go
+++ internal/domain/material/discipline.go
@@ -1,6 +1,25 @@
 package material
 
+import "errors"
+
 type Discipline struct {
 	Id   int
 	Name string // e.g. "Основы Информатики"
+}
+
+func NewDiscipline(id int, name string) (Discipline, error) {
+	if id < 0 {
+		return Discipline{}, errors.New("expected non-negative id")
+	} else if name == "" {
+		return Discipline{}, errors.New("expected non-empty name")
+	}
+	return Discipline{Id: id, Name: name}, nil
+}
+
+func MustNewDiscipline(id int, name string) Discipline {
+	d, err := NewDiscipline(id, name)
+	if err != nil {
+		panic(err)
+	}
+	return d
 }
--- internal/domain/material/material.go
+++ internal/domain/material/material.go
@@ -2,58 +2,64 @@ package material
 
 import (
 	"errors"
-	"fmt"
 	"regexp"
 	"time"
 )
 
 type Material struct {
-	Uuid       string
-	Name       string
-	Desc       string
-	Url        string
-	Author     string
-	Views      int
-	Department Department
-	Discipline Discipline
-	Created    time.Time
+	Uuid         string
+	Name         string
+	Desc         string
+	Url          string
+	Author       *string
+	Views        int
+	DepartmentId int
+	DisciplineId int
+	Created      time.Time
 }
-
-var ErrBadInput = errors.New("bad input")
 
 var uuid_regex *regexp.Regexp = regexp.MustCompile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
 
-func UnmarshalMaterial(uuid, name, desc, url, author string, views int, dept Department, disc Discipline, created_at time.Time) (Material, error) {
+func UnmarshalMaterial(uuid, name, description, url string, author *string,
+	views, department_id, discipline_id int, created_at time.Time) (Material, error) {
 	if !uuid_regex.Match([]byte(uuid)) {
-		return Material{}, fmt.Errorf("%w (%s)", ErrBadInput, "uuid")
+		return Material{}, errors.New("expected valid uuid")
+	} else if name == "" {
+		return Material{}, errors.New("expected non-empty name")
+	} else if url == "" {
+		return Material{}, errors.New("expected non-empty url")
+	} else if department_id < 0 {
+		return Material{}, errors.New("expected non-negative department id")
+	} else if discipline_id < 0 {
+		return Material{}, errors.New("expected non-negative discipline id")
+	} else if views < 0 {
+		return Material{}, errors.New("expected non-negative views")
+	} else if author != nil && *author == "" {
+		return Material{}, errors.New("expected non-empty author name or nil")
 	}
 
-	if name == "" || url == "" {
-		// TODO: we could *theoretically* validate more here
-		return Material{}, fmt.Errorf("%w (%s)", ErrBadInput, "name/url")
-	}
-	if (dept == Department{}) || (disc == Discipline{}) {
-		// TODO: does this work?!
-		return Material{}, fmt.Errorf("%w (%s)", ErrBadInput, "dept/disc")
-	}
-	if views < 0 {
-		return Material{}, fmt.Errorf("%w (%s)", ErrBadInput, "negative views")
-	}
-
-	m := Material{
-		Uuid:       uuid,
-		Name:       name,
-		Desc:       desc, // no validation needed
-		Url:        url,
-		Author:     author, // no validation needed
-		Views:      views,  // all material starts with 0 views
-		Department: dept,
-		Discipline: disc,
-		Created:    created_at,
-	}
-	return m, nil
+	return Material{
+		Uuid:         uuid,
+		Name:         name,
+		Desc:         description,
+		Url:          url,
+		Author:       author,
+		Views:        views,
+		DepartmentId: department_id,
+		DisciplineId: discipline_id,
+		Created:      created_at,
+	}, nil
 }
 
-func NewMaterial(uuid, name, desc, url, author string, dept Department, disc Discipline) (Material, error) {
-	return UnmarshalMaterial(uuid, name, desc, url, author, 0, dept, disc, time.Now())
+// NewMaterial creates a Material with default views and time
+func NewMaterial(uuid, name, desc, url string, author *string, department, discipline int) (Material, error) {
+	return UnmarshalMaterial(uuid, name, desc, url, author, 0, department, discipline, time.Now())
+}
+
+func MustNewMaterial(uuid, name, desc, url string, author *string, department, discipline int) Material {
+	m, err := NewMaterial(uuid, name, desc, url, author, department, discipline)
+	if err != nil {
+		panic(err)
+	}
+	return m
 }
